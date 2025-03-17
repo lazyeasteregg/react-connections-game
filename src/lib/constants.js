4404a724b7d7b020724e1479fd7a130d1887bd1d
Lazy Easter Egg <lazyeasteregg@gmail.com>
2025-03-17 16:35:41 -0700

    changed time and added data

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,2 +1,2 @@
-export const GAME_TITLE = "Black Connections";
+export const GAME_TITLE = "Lazy Easter Egg 2025";
 export const MAX_MISTAKES = 4;
--- src/lib/original-data.js
+++ src/lib/original-data.js
@@ -1,30 +1,75 @@
 export const CONNECTION_GAMES = [
   [
     {
-      category: "colors",
-      words: ["Green", "Blue", "Yellow", "Pink"],
+      category: "Black Women Authors",
+      words: ["Toni", "Paule", "Zora", "Alice"],
       difficulty: 1,
       imageSrc: "https://i.ibb.co/Gn36Ypb/Connections-1.png",
     },
     {
-      category: "Tomb _______",
-      words: ["Raiders", "Stone", "Empty", "Robber"],
+      category: "Michael ____",
+      words: ["Jackson", "Jordan", "Johnson", "Tyson"],
       difficulty: 2,
       imageSrc: "https://i.ibb.co/3B44zXX/Connections-2.png",
     },
     {
-      category: "Peters",
-      words: ["Pan", "Parker", "Simon", "Rabbit"],
+      category: "Black Greek Sorority Symbols",
+      words: ["Poodle", "Dove", "Ivy", "Pyramid"],
       difficulty: 3,
       imageSrc: "https://i.ibb.co/4MwD7zv/Connections-3.png",
     },
 
     {
-      category: "Easter Candy",
-      words: ["Eggs", "Peeps", "Chocolate", "Cadbury"],
+      category: "Boyz II Men",
+      words: ["Michael", "Nathan", "Wanyá", "Shawn"],
       difficulty: 4,
       imageSrc:
         "https://i.ibb.co/V2vnr5P/boys2men-2-d53afdddf480401fac1204a3954aa5f6.webp",
+    },
+  ],
+  [
+    {
+      category: "Countries in West Africa",
+      words: ["Nigeria", "Cameroon", "Togo", "Benin"],
+      difficulty: 1,
+    },
+    {
+      category: "Countries in East Africa",
+      words: ["Tanzania", "Kenya", "Ethiopia", "Somalia"],
+      difficulty: 2,
+    },
+    {
+      category: "Countries in North Africa",
+      words: ["Morocco", "Algeria", "Tunisia", "Libya"],
+      difficulty: 3,
+    },
+    {
+      category: "African Islands",
+      words: ["Madagascar", "Mauritius", "Comoros", "Mayotte"],
+      difficulty: 4,
+    },
+  ],
+  [
+    {
+      category: "California Rappers",
+      words: ["O'Shea", "Eric", "Lesane", "Todd"],
+      difficulty: 1,
+    },
+    {
+      category: "Black Founded Clothing Brands",
+      words: ["Fubu", "Phat Farm", "Enyce", "Rocawear"],
+      difficulty: 2,
+    },
+    {
+      category: "Hair Cut Styles",
+      words: ["Ceasar", "Wavelength", "Fade", "Taper"],
+      difficulty: 3,
+    },
+    {
+      category: "Black Mayors",
+      words: ["Bass", "Turner", "Adams", "Washington"],
+      difficulty: 4,
+      imageSrc: "https://i.ibb.co/pP1xn0Z/Connections.png",
     },
   ],
   [
