368b5e6c331bea7db7da138b838a555465daafe4
lazyeasteregg <34332730+lazyeasteregg@users.noreply.github.com>
2025-04-19 23:18:27 -0700

    Update data.js

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,109 +1,71 @@
 export const CONNECTION_GAMES = [
   [
     {
-      category: "colors",
-      words: ["Green", "Blue", "Yellow", "Pink"],
-      difficulty: 1,
-      imageSrc: "https://i.ibb.co/Gn36Ypb/Connections-1.png",
-    },
-    {
-      category: "Tomb _______",
-      words: ["Raiders", "Stone", "Empty", "Robber"],
-      difficulty: 2,
-      imageSrc: "https://i.ibb.co/3B44zXX/Connections-2.png",
-    },
-    {
-      category: "Peters",
-      words: ["Pan", "Parker", "Simon", "Rabbit"],
-      difficulty: 3,
-      imageSrc: "https://i.ibb.co/4MwD7zv/Connections-3.png",
-    },
-
-    {
-      category: "Easter Candy",
-      words: ["Eggs", "Peeps", "Chocolate", "Cadbury"],
-      difficulty: 4,
-      imageSrc:
-        "https://i.ibb.co/V2vnr5P/boys2men-2-d53afdddf480401fac1204a3954aa5f6.webp",
-    },
-  ],
-  [
-    {
-      category: "Church Leadership Roles",
-      words: ["Deacon", "Elder", "Minister", "Pastor"],
-      difficulty: 1,
-      imageSrc: "https://d1isxmzxwhhjbk.cloudfront.net/3/Church-Roles.png",
-    },
-    {
-      category: "Synonyms for Lucky",
-      words: ["Blessed", "Fortunate", "Favored", "Providential"],
-      difficulty: 2,
-      imageSrc:
-        "https://d1isxmzxwhhjbk.cloudfront.net/3/Synonyms-for-Lucky.png",
-    },
-    {
-      category: "R&B Icons",
-      words: ["Tyrese", "Usher", "Brandy", "Maxwell"],
-      difficulty: 3,
-      imageSrc: "https://d1isxmzxwhhjbk.cloudfront.net/3/R-B-Icons.png",
-    },
-
-    {
-      category: "Tupac's Movie Character Names",
-      words: ["Bishop", "Lucky", "Birdie", "Tank"],
-      difficulty: 4,
-      imageSrc: "https://d1isxmzxwhhjbk.cloudfront.net/3/Tupac-Movie-Roles.png",
-    },
-  ],
-  [
-    {
-      category: "Slang for 'A Lot'",
-      words: ["Bare", "Mad", "Hella", "Buku"],
+      category: "Basketball",
+      words: ["Hoop", "Dribble", "Foul", "Net"],
       difficulty: 1,
     },
     {
-      category: "Slang for 'Great'",
-      words: ["Cold", "Raw", "Dope", "Lit"],
+      category: "Easter _______",
+      words: ["Lily", "Bunny", "Basket", "Service"],
       difficulty: 2,
     },
     {
-      category: "Ways to Refer to Money",
-      words: ["Brick", "Band", "Rack", "C-Note"],
+      category: "Bay Area",
+      words: ["Fog", "Cable Car", "Bridge", "Tech"],
       difficulty: 3,
     },
 
     {
-      category: "Things Needed to Make a Pizza",
-      words: ["Cheddar", "Dough", "Sauce", "Heat"],
+      category: "Travel",
+      words: ["Passport", "Luggage", "Ticket", "Airport"],
       difficulty: 4,
     },
   ],
   [
     {
-      category: "Boondocks Characters",
-      words: ["Riley", "Huey", "Jazmine", "Robert"],
+      category: "Bay Area Hikes",
+      words: ["Dipsea", "Quicksilver", "Mission Peak", "Crosstown"],
       difficulty: 1,
-      imageSrc:
-        "https://d1isxmzxwhhjbk.cloudfront.net/4/Boondocks-Characters.png",
     },
     {
-      category: "Uncle ____",
-      words: ["Ruckus", "Tom", "Luke", "Ben's"],
+      category: "Bay Area Athletes",
+      words: ["Curry", "Purdy", "Young", "Rice"],
       difficulty: 2,
-      imageSrc: "https://d1isxmzxwhhjbk.cloudfront.net/4/Uncle___.png",
     },
     {
-      category: "Bobby ____",
-      words: ["Brown", "Womack", "Schmurda", "Valentino"],
+      category: "Tokyo",
+      words: ["Shibuya", "Sushi", "Shinjuku", "Bullet Train"],
       difficulty: 3,
-      imageSrc: "https://d1isxmzxwhhjbk.cloudfront.net/4/Bobby___.png",
+    },
+
+    {
+      category: "Asian Food",
+      words: ["Hot Pot", "Pho", "Pad Thai", "Dim Sum"],
+      difficulty: 4,
+    },
+  ],
+  [
+    {
+      category: "Chen words",
+      words: ["Trust", "Bet", "Cap", "Dip"],
+      difficulty: 1,
     },
     {
-      category: "Black Panther Leadership",
-      words: ["Bobby", "Fred", "Elaine", "Kathleen"],
-      difficulty: 4,
+      category: "Yes",
+      words: ["Yuh", "Ok", "No Yeah", "Down"],
+      difficulty: 2,
+    },
+    {
+      category: "Cool",
+      words: ["Fire", "Slaps", "Rad", "Bussin"],
+      difficulty: 3,
+    },
 
-      imageSrc: "https://d1isxmzxwhhjbk.cloudfront.net/4/Black-Panthers.png",
+    {
+      category: "Sucks",
+      words: ["Cringe", "Lame", "Mid", "Meh"],
+      difficulty: 4,
     },
   ],
 ];
